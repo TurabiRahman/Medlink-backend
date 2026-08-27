a8e8e35d32e2f762597501a777830998136a4b91
TurabiRahman <rahman.turabi.10@gmail.com>
2026-08-27 18:29:57 +0600

    feat: generated a universal password

--- server/generate-password-hashes.js
+++ server/generate-password-hashes.js
@@ -6,10 +6,12 @@ const passwords = {
   // emergency: 'EmergencyAdmin123!'
 
   // new ambulance admin passwords
-  dhakaAmbulance: 'DhakaAmbulance123!',
-  medlifeAmbulance: 'MedLifeAmbulance123!',
-  uttaraAmbulance: 'UttaraAmbulance123!',
-  mirpurAmbulance: 'MirpurAmbulance123!'
+  // dhakaAmbulance: 'DhakaAmbulance123!',
+  // medlifeAmbulance: 'MedLifeAmbulance123!',
+  // uttaraAmbulance: 'UttaraAmbulance123!',
+  // mirpurAmbulance: 'MirpurAmbulance123!'
+
+  newPass: 'SecurePass123!'  // this will be the password for all the customers, ambulance_admins, hospital_admins, and super_admins. It will be changed later by the users themselves.
 
 };
 
